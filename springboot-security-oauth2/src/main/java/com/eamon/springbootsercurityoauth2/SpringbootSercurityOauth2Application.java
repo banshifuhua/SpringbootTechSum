39929b61ed488d23c068b3596602055c20ae4d76
eamon.zhang <1qaz@WSX#EDC>
2018-10-19 16:53:18 +0800

    初始化  springboot oauth2 项目

---
+++ springboot-security-oauth2/src/main/java/com/eamon/springbootsercurityoauth2/SpringbootSercurityOauth2Application.java
@@ -0,0 +1,12 @@
+package com.eamon.springbootsercurityoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSercurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSercurityOauth2Application.class, args);
+    }
+}
