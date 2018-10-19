56d1cb74040010ceb0ddf4211202c75a862fed5c
eamon.zhang <1qaz@WSX#EDC>
2018-10-19 15:36:16 +0800

    初始化 多模块项目

---
+++ springboot-security-simple/src/main/java/com/eamon/securitysimple/SecuritySimpleApplication.java
@@ -0,0 +1,12 @@
+package com.eamon.securitysimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySimpleApplication.class, args);
+	}
+}
