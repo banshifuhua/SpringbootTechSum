ed92945f6a1d9dafa5a91f8fa5fc01a3c40a3766
eamon.zhang <1qaz@WSX#EDC>
2019-01-02 15:46:09 +0800

    初始化 整合 ElasticSearch 项目结构

---
+++ springboot-elasticsearch-simple/src/main/java/com/eamon/springbootelasticsearchsimple/SpringbootElasticsearchSimpleApplication.java
@@ -0,0 +1,14 @@
+package com.eamon.springbootelasticsearchsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootElasticsearchSimpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootElasticsearchSimpleApplication.class, args);
+    }
+
+}
+
