48a3fd79571cd2cf1d336270e8790cfed3777ae9
eamon.zhang <>
2019-08-09 13:25:53 +0800

    添加 工程文件

---
+++ springboot-rabbitmq-delay-quque/src/main/java/com/eamon/springbootrabbitmqdelayquque/SpringbootRabbitmqDelayQuqueApplication.java
@@ -0,0 +1,13 @@
+package com.eamon.springbootrabbitmqdelayquque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqDelayQuqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqDelayQuqueApplication.class, args);
+	}
+
+}
---
+++ springboot-rabbitmq-delay-quque/src/test/java/com/eamon/springbootrabbitmqdelayquque/SpringbootRabbitmqDelayQuqueApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eamon.springbootrabbitmqdelayquque;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRabbitmqDelayQuqueApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
